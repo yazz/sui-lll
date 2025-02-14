1ef80ee4855dce140aee53de0173543235006014
Rene <rene@mccaine.me>
2025-02-14 14:20:17 +0100

    test connected acc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   ConnectButton,
   SuiClientProvider,
+  useCurrentAccount,
+  useSuiClientQuery,
   WalletProvider,
 } from "@mysten/dapp-kit";
 import { getFullnodeUrl } from "@mysten/sui/client";
 import GlobalStyles from "./styles/GlobalStyles";
 
-import ProgressBar from "./components/ProgressBar";
 import { lightTheme } from "./styles/theme";
 
 const queryClient = new QueryClient();
@@ -23,13 +24,13 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider networks={networks} defaultNetwork="devnet">
         <WalletProvider>
-          <ThemeProvider theme={lightTheme}>
-            <GlobalStyles />
-            <Main>
-              <ConnectButton />
-              <ProgressBar value={20} my={4} px={10} />
-            </Main>
-          </ThemeProvider>
+          {/* <ThemeProvider theme={lightTheme}> */}
+          {/* <GlobalStyles /> */}
+          <Main>
+            <ConnectButton />
+            <ConnectedAccount />
+          </Main>
+          {/* </ThemeProvider> */}
         </WalletProvider>
       </SuiClientProvider>
     </QueryClientProvider>
@@ -45,3 +46,41 @@ const Main = styled.main`
 `;
 
 export default App;
+
+function ConnectedAccount() {
+  const account = useCurrentAccount();
+
+  if (!account) {
+    return <div>No account connected</div>;
+  }
+
+  return (
+    <div>
+      <div>Connected to {account.address}</div>;
+      <OwnedObjects address={account.address} />
+    </div>
+  );
+}
+
+function OwnedObjects({ address }) {
+  const { data } = useSuiClientQuery("getOwnedObjects", {
+    owner: address,
+  });
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {data.data.map((object) => (
+        <li key={object.data?.objectId}>
+          <a
+            href={`https://example-explorer.com/object/${object.data?.objectId}`}
+          >
+            {object.data?.objectId}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
